68ccf8773175771dac03086ac29c79be2e58834c
CarloEz <marcarlos47@gmail.com>
2020-10-11 01:06:05 -0600

    heroku v1
    morgan

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ const express=require('express');
 const PORT=3000|| process.env.PORT;
 const cors=require('cors');
 const app= express();
-const morgan=require('morgan');
+//const morgan=require('morgan');
 const {connection} =require('./database/db');
 
-app.use(morgan('dev'));
+//app.use(morgan('dev'));
 app.use(cors({}));
 
 app.use(express.json());
